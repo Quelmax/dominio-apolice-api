64c2c11ac6ec3992485f37f99e5c0d1ff849cfb4
quelmax <quemuelvital@live.com>
2025-04-21 01:22:52 -0300

    ajustando interface para herdar de ABC

--- app/repositories/IApoliceRepository.py
+++ app/repositories/IApoliceRepository.py
@@ -1,9 +1,11 @@
+from abc import ABC, abstractmethod
 from app.domain.apolice import Apolice
 
-class IApoliceRepository:
+class IApoliceRepository(ABC):
+    @abstractmethod
     def salvar(self, apolice: Apolice) -> None:
         """
-        Salva a apólice no repositório. 
+        Salva a apólice no repositório.
         :param apolice: A apólice a ser salva.
         """
-        raise NotImplementedError("Esse método deve ser implementado na subclasse.")
+        pass
